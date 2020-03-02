2f9d528c4fbbd4c6128941be0ae74c248d18f85f
Carcharocles-megalodon <55915879+Carcharocles-megalodon@users.noreply.github.com>
2020-03-02 21:30:46 +0000

    1st commit

---
+++ DJDStack/DJDStack/DJDStack.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APAIProjs
+{
+    class DJDStack
+    {
+        int index = 0;
+        int _size = 0;
+        int[] stack;
+         public DJDStack(int size)
+        {
+            _size = size;
+            stack = new int[_size];
+        }
+        public DJDStack()
+        {
+            stack = new int[6];
+        }
+
+        public void Push(int add)
+        {
+            if (index>= stack.Length)
+            {
+                Console.WriteLine("It's too big, it won't fit!");
+            }
+            else
+            {
+                index++;
+                stack[index] = add;
+            }
+
+        }
+        public int Pop()
+        {
+            if(index <= 0)
+            {
+                Console.WriteLine("Too small, can't even feel it.");
+                return 0;
+            }
+            else
+            {
+                return stack[index--];
+            }
+        }
+        public int Peek()
+        {
+            if (index <= 0)
+            {
+                Console.WriteLine("Nothing there anymore, mate, it's empty.");
+                return 0;
+            }
+            else
+            {
+                return stack[index];
+            }
+        }
+    }
+}
+
---
+++ DJDStack/DJDStack/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace APAIProjs
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Queue Q = new Queue(10);
+            for(int i = 0; i < 10; i++)
+            {
+                Q.Enqueue(i);
+            }
+            Console.WriteLine(Q.Dequeue());
+            Console.WriteLine(Q.Dequeue());
+            Console.WriteLine(Q.Empty(0));
+            Console.WriteLine(Q.Empty(1));
+        }
+    }
+}

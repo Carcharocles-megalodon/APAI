3063cfdf0d94af33f2e76e9c482cdd111809546c
Carcharocles-megalodon <55915879+Carcharocles-megalodon@users.noreply.github.com>
2020-04-24 12:36:55 +0100

    4a tarefa

---
+++ InvertedAPAIEx/InvertedAPAIEx/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace InvertedAPAIEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllText("lines.txt").Split('.', StringSplitOptions.RemoveEmptyEntries);
+            string[] inverted = new string[lines.Length];
+            int j = 0;
+            while (j < lines.Length-1)
+            {
+                for (int i = lines.Length; i > 0; i--)
+                {
+                    inverted[j] = lines[i];
+                    j++;
+                }
+            }
+            Console.WriteLine(inverted);
+            File.WriteAllLines("lines.txt", inverted);
+        }
+    }
+}

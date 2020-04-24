821ecbd5b2ca1039709c5c86a0ae9f81f0bcc845
Carcharocles-megalodon <55915879+Carcharocles-megalodon@users.noreply.github.com>
2020-04-24 18:21:01 +0100

    Anagram

---
+++ AnagramTarefa/AnagramTarefa/Anagram.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnagramTarefa
+{
+    class Anagram
+    {
+        public string word1;
+        public string word2;
+
+
+        public Anagram(string word1, string word2)
+        {
+            this.word1 = word1;
+            this.word2 = word2;
+        }
+        public bool IsItAnagram()
+        {
+            var word1Char = word1.ToLower().ToCharArray();
+            var word2Char = word2.ToLower().ToCharArray();
+
+            Array.Sort(word1Char);
+            Array.Sort(word2Char);
+
+            return word1Char == word2Char;
+        }
+    }
+}
---
+++ AnagramTarefa/AnagramTarefa/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AnagramTarefa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Anagram a;
+
+            string command = Console.ReadLine();
+            while(command != "End")
+            {
+                string[] separate = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                a = new Anagram(separate[0], separate[1]);
+                Console.WriteLine(a.IsItAnagram());
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
